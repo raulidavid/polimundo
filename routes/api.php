558c9075c8c89d5866ca533092f196a7b4aac64c
Raúl Flores <raulidavid@hotmail.com>
2022-07-18 22:52:22 +0000

    ADD FIND PERSON

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/persona', function () {
+    return User::all();
+});
+
+Route::get('/persona/{id}', function ($id) {
+    return User::find($id);
 });

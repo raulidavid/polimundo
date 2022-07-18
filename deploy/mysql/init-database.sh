2a54bdcd5ffde0645cca3e8ac59f52245dcb8d46
Raúl Flores <raulidavid@hotmail.com>
2022-07-18 18:11:19 -0500

    FIXED INITDB

--- deploy/mysql/init-database.sh
+++ deploy/mysql/init-database.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-mysql -u root -proot sandbox < "/docker-entrypoint-initdb.d/000-create-databases.sql"
-mysql -u root -proot sandbox < "/docker-entrypoint-initdb.d/001-create-tables.sql"
+mysql -u root -proot laravel < "000-create-databases.sql"
